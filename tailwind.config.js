caac956cf2b7643fe8caec28fe5f3386377b9e19
amrullahmukhtar <amrullahmukhtar@gmail.com>
2024-03-28 10:40:52 +0700

    edit nested css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+import postcss from "postcss";
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
-
+  plugins: [postcss],
+};
