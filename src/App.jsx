84376c567a2ba45328e90db442ad18a80b779e0e
amrullahmukhtar <amrullahmukhtar@gmail.com>
2024-04-01 09:31:40 +0700

    a

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
         <Route path="/myProject" element={<MyProject />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
       </Routes>
+      {/*  */}
     </>
   );
 }
